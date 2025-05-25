408bd387a9778fd88c0007b9dafde397467861ea
Santi292003 <s.ramirez9@utp.edu.co>
2025-05-25 09:25:48 -0500

    Proyecto conectado y finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 // import { useState } from 'react'
-import StickyHeadTable from './components/StickyHeadTable';
-import { Box, Typography } from '@mui/material';
+import StickyHeadTable from "./components/StickyHeadTable";
+import { Box, Typography } from "@mui/material";
 
 function App() {
-  const [emails, setEmails] = useState({"mails":[{"from":"Maria José Medina Ruiz","date":"2025-05-20","subject":"Prueba de correo","summary":"Hola, mi nombre es Maria José, estoy probando este contacto"}]});
+  const [emails, setEmails] = useState({ mails: [] });
 
-  // const fetchEmails = async () => {
-  //   try {
-  //     const response = await fetch('https://tu-n8n/webhook/emails');
-  //     const data = await response.json();
-  //     setEmails(data);
-  //   } catch (error) {
-  //     console.error('Error al obtener correos:', error);
-  //   }
-  // };
+  const fetchEmails = async () => {
+    try {
+      const response = await fetch(
+        "https://wise-teaching-bat.ngrok-free.app/webhook-test/emails",
+        {
+          headers: {
+            "ngrok-skip-browser-warning": "true",
+          },
+        }
+      );
+      const data = await response.json();
+      setEmails(data);
+    } catch (error) {
+      console.error("Error al obtener correos:", error);
+    }
+  };
 
   useEffect(() => {
-    setEmails({
-    "mails":
-    [
-      {"from":"Maria José Medina Ruiz","date":"2025-05-20","subject":"Prueba de correo","summary":"Hola, mi nombre es Maria José, estoy probando este contacto"},
-      {"from":"Maria José Medina Ruiz","date":"2025-05-21","subject":"Prueba de correo2","summary":"Hola, podrias darme información sobre esta página?"},
-      {"from":"Maria José Medina Ruiz","date":"2025-05-20","subject":"Prueba de correo3","summary":"Hola, solicito documentos"}
-    ]});
-    // fetchEmails();
-
-    // const interval = setInterval(fetchEmails, 30000);
-    // return () => clearInterval(interval);
+    fetchEmails();
   }, []);
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         padding: 4,
       }}
     >
       <Typography
-          variant="h3"
-          component="h1"
-          gutterBottom
-          sx={{
-            fontFamily: "'Montserrat', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
-          }}
-        >
+        variant="h3"
+        component="h1"
+        gutterBottom
+        sx={{
+          fontFamily:
+            "'Montserrat', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+        }}
+      >
         MANEJADOR DE MAILS
       </Typography>
       <StickyHeadTable rows={emails.mails} />
